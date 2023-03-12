0ed05a1d01461a2e282212dd83fd3343351f7d04
Jory Shilmover <jory.shilmover@gmail.com>
2023-03-12 11:55:55 -0700

    changed to use layout file and installed breeze

--- crud-app/routes/web.php
+++ crud-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,8 +9,8 @@ use App\Http\Controllers\ProductController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -21,3 +21,15 @@ Route::get('/', function () {
 Route::resource('products', 'App\Http\Controllers\ProductController');
 Route::resource('review', "App\Http\Controllers\ReviewController");
 Route::resource('users', "App\Http\Controllers\UserController");
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
